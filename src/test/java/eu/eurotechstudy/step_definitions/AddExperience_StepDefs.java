94df3074d87c6224325a957e9ba18420244c6f58
Blagoja <blagojaan@yahoo.com>
2025-10-15 21:10:14 +0200

    first commit

---
+++ src/test/java/eu/eurotechstudy/step_definitions/AddExperience_StepDefs.java
@@ -0,0 +1,38 @@
+package eu.eurotechstudy.step_definitions;
+
+import eu.eurotechstudy.pages.AddExperiencePage;
+import eu.eurotechstudy.pages.MyProfilePage;
+import eu.eurotechstudy.pages.OverviewPage;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.StaleElementReferenceException;
+
+public class AddExperience_StepDefs {
+
+    MyProfilePage myProfilePage = new MyProfilePage();
+    AddExperiencePage addExperiencePage = new AddExperiencePage();
+    OverviewPage overviewPage = new OverviewPage();
+
+    @When("user clicks on the {string} section")
+    public void user_clicks_on_the_section(String section) {
+        myProfilePage.getSectionByName(section).click();
+    }
+    @When("user enters {string} {string} in Add Experience")
+    public void user_enters_in_add_experience(String field, String value) {
+        addExperiencePage.setFieldByName(field,value);
+    }
+    @When("user clicks on the Add Experience Button")
+    public void user_clicks_on_the_add_experience_button() {
+        addExperiencePage.addExperienceButton.click();
+    }
+    @Then("verify that user sees experience details on the Overview section")
+    public void verify_that_user_sees_experience_details_on_the_overview_section() {
+        try{
+            Assert.assertEquals("SDET",overviewPage.firstJobTitle.getText());
+        }catch (StaleElementReferenceException s){
+            Assert.assertEquals("SDET",overviewPage.firstJobTitle.getText());
+        }
+
+    }
+}
